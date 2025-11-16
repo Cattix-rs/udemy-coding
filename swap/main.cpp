5b2ea6a367e65a66d6f135612b0435b97bd71004
Cattix-rs <ruben.stolwijk@gmail.com>
2025-11-16 19:41:30 +0100

    Create main.cpp

---
+++ swap/main.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+
+int main()
+{
+	int a;
+	int b;
+
+	a = 3;
+	b = 5;
+	std::cout << "inta " << a << std::endl;
+	std::cout << "intb " << b << std::endl;
+
+	void swap(int* a, int* b);
+
+	std::cout << "int new a " << a << std::endl;
+	std::cout << "int new b " << b << std::endl;
+
+
+
+
+
+	return 0;
+}
