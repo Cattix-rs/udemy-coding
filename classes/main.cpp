56a487e6d5bee95c6f2ca6769203c70355d18417
Cattix-rs <ruben.stolwijk@gmail.com>
2025-12-23 10:10:35 +0100

    lab Explicit constructors
    and some clean up for readablity in the class name / arguments etc.

--- classes/main.cpp
+++ classes/main.cpp
@@ -3,23 +3,38 @@
 class Entity
 {
 public:
-	Entity(int hitpoint = 100); // parameterized constructor.
+	explicit Entity(int hitPoint = 100); // parameterized constructor.
+
+	int getHitpoints() const
+	{
+		return hitPoints;
+	}
 
 private:
-	int hitpoints = 0;
+	int hitPoints = 0;
 
 };
 
-Entity::Entity(int hitpoints)
-	: hitpoints(hitpoints)
+Entity::Entity(int hitPoints)
+	: hitPoints(hitPoints)
 {
-	std::cout << "Entity::Entity(" << this->hitpoints << ")" << std::endl;
+	std::cout << "Entity::Entity(" << this->hitPoints << ")" << std::endl;
+}
+
+void printHitPoints(const Entity& e)
+{
+	std::cout << "hitPoints: " << e.getHitpoints() << std::endl;
 }
 
 int main()
 {
 	Entity entity{ 50 };
 
+	printHitPoints(entity);
+
+	printHitPoints(Entity{ 10 });
+
+	Entity entity2 = Entity{ 100 }; // this is valid because we have a paramtatized constructor
 
 	return 0;
 }
