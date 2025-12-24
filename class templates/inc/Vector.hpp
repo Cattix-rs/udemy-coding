d7d83dc8538e2ee4e1962b06f365c17b08fca7cc
Cattix-rs <ruben.stolwijk@gmail.com>
2025-12-24 15:43:44 +0100

    worked on more template class
    errror copied code form lab

--- class templates/inc/Vector.hpp
+++ class templates/inc/Vector.hpp
@@ -1,9 +1,16 @@
 #pragma once
+#include <cassert>
 
 template<typename T, size_t N>
 struct Vector
 {
 	Vector<T, N> operator+(const Vector<T, N>& rhs) const;
+
+	/*T& operator[](size_t i);
+	const T& operator[](size_t i) const;*/
+
+	template<typename Self>
+	auto&& operator[](this Self&& self, size_t i)
 
 	T data[N];
 };
@@ -19,6 +26,29 @@ Vector<T, N> Vector<T, N>::operator+(const Vector<T, N>& rhs) const
 	}
 	return res;
 }
+
+template <typename T, size_t N>
+template<typename Self>
+auto&& Vector<T, N>::operator[](Self&& self, size_t i)
+	{
+	assert(i < N);
+	return self.data[i];
+	}
+
+
+
+//template <typename T, size_t N>
+//T& Vector<T, N>::operator[](size_t i)
+//{
+//	assert(i < N);
+//	return data[i];
+//}
+//template <typename T, size_t N>
+//const T& Vector<T, N>::operator[](size_t i) const
+//{
+//	assert(i < N);
+//	return data[i];
+//}
 
 using vec2f = Vector<float, 2>;
 using vec3f = Vector<float, 3>;
